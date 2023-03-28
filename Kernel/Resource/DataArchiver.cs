119cabceeec5f0391eda438464f2a108c2746604
Neo <wtzd8645@gmail.com>
2023-03-29 01:30:55 +0800

    Add .Net Core project

---
+++ Kernel/Resource/DataArchiver.cs
@@ -0,0 +1,92 @@
+﻿using Blanketmen.Hypnos.Compression;
+using Blanketmen.Hypnos.Encryption;
+using Blanketmen.Hypnos.Serialization;
+
+namespace Blanketmen.Hypnos
+{
+    public class DataArchiver
+    {
+        private readonly ISerializer serializer;
+        private readonly ICompressor compressor;
+        private readonly IEncryptor encryptor;
+
+        public DataArchiver(ISerializer serializer, ICompressor compressor = null, IEncryptor encryptor = null)
+        {
+            this.serializer = serializer;
+            this.compressor = compressor;
+            this.encryptor = encryptor;
+        }
+
+        public void Save<T>(T obj, string filePath)
+        {
+            try
+            {
+                byte[] data = serializer.Serialize(obj);
+                if (compressor != null)
+                {
+                    data = compressor.Compress(data, 0, data.Length);
+                }
+
+                if (encryptor != null)
+                {
+                    data = encryptor.Encrypt(data, 0, data.Length);
+                }
+
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.Read))
+                {
+                    fileStream.Write(data, 0, data.Length);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public T Load<T>(string filePath, object encryptionKey = null)
+        {
+            try
+            {
+                byte[] data;
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    int offset = 0;
+                    if (fileStream.Length > int.MaxValue)
+                    {
+                        throw new IOException("File length exceeds 2GB");
+                    }
+
+                    int unreadLength = (int)fileStream.Length;
+                    data = new byte[unreadLength];
+                    while (unreadLength > 0)
+                    {
+                        int readLength = fileStream.Read(data, offset, unreadLength);
+                        if (readLength == 0)
+                        {
+                            throw new EndOfStreamException("Read beyond EOF");
+                        }
+
+                        offset += readLength;
+                        unreadLength -= readLength;
+                    }
+                }
+
+                if (encryptor != null)
+                {
+                    data = encryptor.Decrypt(data, 0, data.Length);
+                }
+
+                if (compressor != null)
+                {
+                    data = compressor.Decompress(data, 0, data.Length);
+                }
+
+                return serializer.Deserialize<T>(data);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Kernel/Resource/ResourceManager.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Blanketmen.Hypnos
+{
+    public sealed class ResourceManager
+    {
+        #region Singleton
+        public static ResourceManager Instance { get; private set; }
+
+        public static void CreateInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new ResourceManager();
+            }
+        }
+
+        public static void ReleaseInstance()
+        {
+            Instance = null;
+        }
+
+        private ResourceManager() { }
+        #endregion
+
+        public static string SaveFileExt { get; private set; }
+        public static string BackupFileExt { get; private set; }
+        public static string PersistentDataPath { get; private set; }
+
+        private readonly Dictionary<int, DataArchiver> dataArchivers = new Dictionary<int, DataArchiver>(3);
+
+        public void Initialize(ResourceConfig config)
+        {
+            SaveFileExt = config.saveFileExt;
+            BackupFileExt = config.backupFileExt;
+            PersistentDataPath = config.persistentDataPath;
+            for (int i = 0; i < config.dataArchiverConfigs.Length; ++i)
+            {
+                DataArchiverConfig archiverConfig = config.dataArchiverConfigs[i];
+                DataArchiver archiver = new DataArchiver(
+                    CoreUtil.CreateSerializer(archiverConfig.serializer),
+                    CoreUtil.CreateCompressor(archiverConfig.compressor),
+                    CoreUtil.CreateEncryptor(archiverConfig.encryptor));
+                dataArchivers.Add(archiverConfig.id, archiver);
+            }
+        }
+
+        public void SaveArchive<T>(int archiverId, T obj, string filePath)
+        {
+            dataArchivers.TryGetValue(archiverId, out DataArchiver archiver);
+            if (archiver == null)
+            {
+                Kernel.LogError($"[ResourceManager] DataArchiver is null. Id: {archiverId}");
+                return;
+            }
+
+            filePath = Path.Combine(PersistentDataPath, filePath);
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            BackUp(filePath);
+
+            try
+            {
+                archiver.Save(obj, filePath + SaveFileExt);
+            }
+            catch (Exception e)
+            {
+                Kernel.LogError($"[ResourceManager] Save file failed. Exception: {e.Message}");
+                Restore(filePath);
+            }
+        }
+
+        private void BackUp(string filePath)
+        {
+            try
+            {
+                File.Copy(filePath + SaveFileExt, filePath + BackupFileExt, true);
+            }
+            catch (FileNotFoundException)
+            {
+                // NOTE: No backup for the first save.
+                return;
+            }
+            catch (Exception e)
+            {
+                Kernel.LogError($"[ResourceManager] Back up file failed. Exception: {e.Message}");
+            }
+        }
+
+        private void Restore(string filePath)
+        {
+            try
+            {
+                File.Copy(filePath + BackupFileExt, filePath + SaveFileExt, true);
+            }
+            catch (Exception e)
+            {
+                Kernel.LogError($"[ResourceManager] Restore file failed. Exception: {e.Message}");
+            }
+        }
+
+        public T LoadArchive<T>(int archiverId, string filePath)
+        {
+            dataArchivers.TryGetValue(archiverId, out DataArchiver archiver);
+            if (archiver == null)
+            {
+                Kernel.LogError($"[ResourceManager] DataArchiver is null. Id: {archiverId}");
+                return default;
+            }
+
+            filePath = Path.Combine(PersistentDataPath, filePath);
+            try
+            {
+                return archiver.Load<T>(filePath + SaveFileExt);
+            }
+            catch (Exception e)
+            {
+                Kernel.LogError($"[ResourceManager] Load file failed, use backup instead. Exception: {e.Message}");
+                return LoadBackup<T>(archiver, filePath);
+            }
+        }
+
+        private T LoadBackup<T>(DataArchiver archiver, string filePath)
+        {
+            try
+            {
+                return archiver.Load<T>(filePath + BackupFileExt);
+            }
+            catch (Exception e)
+            {
+                Kernel.LogError($"[ResourceManager] Load backup file failed. Exception: {e.Message}");
+                return default;
+            }
+        }
+    }
+}
